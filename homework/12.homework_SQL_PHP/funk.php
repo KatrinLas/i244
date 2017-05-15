79e1bd9a56a168e3c3a12989a9863118f608e0a5
Katrin Lasberg <klasberg@itcollege.ee>
2017-05-15 17:17:36 +0300

    13.homework

--- homework/12.homework_SQL_PHP/funk.php
+++ homework/12.homework_SQL_PHP/funk.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function connect_db(){
 	global $connection;
 	$host="localhost";
@@ -13,6 +12,35 @@ function connect_db(){
 
 function logi(){
 	// siia on vaja funktsionaalsust (13. nädalal)
+	global $connection;
+	global $errors;
+	if (!empty($_SESSION['user'])){
+		header("Location: ?page=loomad");
+	}
+	else {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$errors = array();
+			if (!empty($_POST['user'])) {
+			}else $errors[] = "Sisestage kasutajanimi";
+        
+			if (!empty($_POST['pass'])) {
+			} else $errors[] = "Sisestage parool";
+			if (empty($errors)) {
+				$kasutaja = mysqli_real_escape_string($connection, $_POST["user"]);
+				$parool = mysqli_real_escape_string($connection, $_POST["pass"]);
+				$sql = "SELECT id FROM klasberg_kylastajad WHERE username = '{$kasutaja}' and passw= SHA1('{$parool}')";
+				$result = mysqli_query($connection, $sql) or die ("ei saa parooli ja kasutajat kontrollitud".mysqli_error($connection));
+				$rida = mysqli_num_rows($result);
+				print_r($rida);
+				if ($rida) {
+					$_SESSION['user'] = $_POST['user'];
+					header("Location: ?page=loomad");
+				} else {
+					header("Location: ?page=login");
+				}	
+            }
+        }
+    }
 
 	include_once('views/login.html');
 }
@@ -26,6 +54,9 @@ function logout(){
 function kuva_puurid(){
 	// siia on vaja funktsionaalsust
 	global $connection;
+	if (empty($_SESSION['user'])) {
+        header("Location: ?page=login");
+    }
     $puurid = array();
     $sql = "SELECT DISTINCT(puur) AS puur FROM klasberg ORDER BY puur ASC";
     $puuride_nr = mysqli_query($connection, $sql) or die ("ei leidnud puuride numbreid");
@@ -42,7 +73,42 @@ function kuva_puurid(){
 
 function lisa(){
 	// siia on vaja funktsionaalsust (13. nädalal)
-	
+	global $connection;
+	global $errors;
+	if (empty($_SESSION['user'])){
+		header("Location: ?page=login");
+	}
+	else {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$errors = array();
+			if (!empty($_POST['nimi'])) {
+			} else $errors[] = "Sisestage loomanimi";
+		
+			if (!empty($_POST['puur'])) {
+			} else $errors[] = "Sisestage puur";
+        
+			if ($liik = upload('liik')) {
+			} else $errors[] = "Lisage fail";
+        
+			if (empty($errors)) {
+				$loomanimi = mysqli_real_escape_string($connection, $_POST["nimi"]);
+				$puurinr = mysqli_real_escape_string($connection, $_POST["puur"]);
+				$fail = mysqli_real_escape_string($connection, $liik);
+				$sql = "INSERT INTO klasberg (nimi, puur, liik) VALUES ('{$loomanimi}','{$puurinr}', '{$fail}')";
+				$result = mysqli_query($connection, $sql) or die ("ei saa looma lisatud".mysqli_error($connection));
+				$rida = mysqli_insert_id($result);
+				#print_r($rida);
+				if ($rida) {
+					$_SESSION['user'] = $_POST['user'];
+					header("Location: ?page=loomad");
+				} else {
+					header("Location: ?page=lisa");
+				}	
+            }
+        }
+    }
+
+		
 	include_once('views/loomavorm.html');
 	
 }
@@ -55,22 +121,23 @@ function upload($name){
 	if ( in_array($_FILES[$name]["type"], $allowedTypes)
 		&& ($_FILES[$name]["size"] < 100000)
 		&& in_array($extension, $allowedExts)) {
-    // fail õiget tüüpi ja suurusega
+	// fail õiget tüüpi ja suurusega
 		if ($_FILES[$name]["error"] > 0) {
-			$_SESSION['notices'][]= "Return Code: " . $_FILES[$name]["error"];
-			return "";
+				$_SESSION['notices'][]= "Return Code: " . $_FILES[$name]["error"];
+				return "";
 		} else {
-      // vigu ei ole
-			if (file_exists("pildid/" . $_FILES[$name]["name"])) {
-        // fail olemas ära uuesti lae, tagasta failinimi
-				$_SESSION['notices'][]= $_FILES[$name]["name"] . " juba eksisteerib. ";
-				return "pildid/" .$_FILES[$name]["name"];
-			} else {
-        // kõik ok, aseta pilt
-				move_uploaded_file($_FILES[$name]["tmp_name"], "pildid/" . $_FILES[$name]["name"]);
-				return "pildid/" .$_FILES[$name]["name"];
-			}
+	// vigu ei ole
+				if (file_exists("pildid/" . $_FILES[$name]["name"])) {
+		// fail olemas ära uuesti lae, tagasta failinimi
+					$_SESSION['notices'][]= $_FILES[$name]["name"] . " juba eksisteerib. ";
+					return "pildid/" .$_FILES[$name]["name"];
+				} else {
+		// kui kõik ok, aseta pilt 6igesse kausta
+					move_uploaded_file($_FILES[$name]["tmp_name"], "pildid/" . $_FILES[$name]["name"]);
+					return "pildid/" .$_FILES[$name]["name"];
+			}	
 		}
+	
 	} else {
 		return "";
 	}
